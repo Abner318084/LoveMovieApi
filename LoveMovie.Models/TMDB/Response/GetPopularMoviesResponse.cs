cb0581663e1d2adc356501148bbaa321f23cb38b
Allen Chen <shark318084@gmail.com>
2020-01-04 12:32:22 +0800

    Seperate request and response

--- LoveMovie.Models/TMDB/Response/GetPopularMoviesResponse.cs
+++ LoveMovie.Models/TMDB/Response/GetPopularMoviesResponse.cs
@@ -5,22 +5,7 @@ using System.Text;
 using System.Text.Json.Serialization;
 
 namespace LoveMovie.Models.TMDB
-{
-    public class GetPopularMoviesRequest : HttpGetRequest
-    {
-        [JsonPropertyName("language")]
-        [RegularExpression(@"([a-z]{2})-([A-Z]{2})")]
-        public string Language { get; set; }
-
-        [JsonPropertyName("page")]
-        [Range(1, 1000)]
-        public int? Page { get; set; }
-
-        [JsonPropertyName("region")]
-        [RegularExpression(@"^[A-Z]{2}$")]
-        public string Region { get; set; }
-    }
-
+{   
     public class GetPopularMoviesResponse : ITmdbPagedList<GetPopularMoviesResult>
     {
         [JsonPropertyName("page")]
