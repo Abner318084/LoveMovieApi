05ecc2e871699f500240042f8c794606ef98ddc5
Allen Chen <shark318084@gmail.com>
2020-01-04 12:17:44 +0800

    Add model validation to "GetPopularMovies"

--- LoveMovie.Models/TMDB/GetPopularMovies.cs
+++ LoveMovie.Models/TMDB/GetPopularMovies.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Text.Json.Serialization;
 
@@ -8,12 +9,15 @@ namespace LoveMovie.Models.TMDB
     public class GetPopularMoviesRequest : HttpGetRequest
     {
         [JsonPropertyName("language")]
+        [RegularExpression(@"([a-z]{2})-([A-Z]{2})")]
         public string Language { get; set; }
 
         [JsonPropertyName("page")]
+        [Range(1, 1000)]
         public int? Page { get; set; }
 
         [JsonPropertyName("region")]
+        [RegularExpression(@"^[A-Z]{2}$")]
         public string Region { get; set; }
     }
 
