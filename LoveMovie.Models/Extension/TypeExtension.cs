e1fb59508220d79f637e3f6e0f36a62782115d84
Allen Chen <allen.chen@devpro.com.tw>
2020-01-26 17:25:27 +0800

    Add extension method of Type -> IsNullableEnum()

---
+++ LoveMovie.Models/Extension/TypeExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LoveMovie.Models.Extension
+{
+    public static class TypeExtension
+    {
+        public static bool IsNullableEnum(this Type t)
+        {
+            Type u = Nullable.GetUnderlyingType(t);
+            return (u != null) && u.IsEnum;
+        }
+    }
+}
